1ff70144348124e876d4108d42cc1d9e9ded6e57
Thomas Keller <tho.keller@unibas.ch>
2020-04-01 11:57:40 +0200

    Add files required to start an experiment

---
+++ prostlab/cached_revision.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Prost Lab uses the Lab package to conduct experiments with the
+# Prost planning system.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import glob
+import os.path
+import subprocess
+
+from lab import tools
+from lab.cached_revision import CachedRevision
+
+
+class CachedProstRevision(CachedRevision):
+    """This class represents Prost checkouts.
+
+    It provides methods for caching and compiling given revisions.
+    """
+
+    def __init__(self, repo, rev, build_options):
+        """
+        * *repo*: Path to Prost repository.
+        * *rev*: Prost revision.
+        * *build_options*: List of build.py options.
+        """
+        super().__init__(repo, rev, ["./build.py"] + build_options, ["scripts"])
+
+    def _cleanup(self):
+        # Strip binaries.
+        binaries = []
+        for path in glob.glob(os.path.join(self.path, "builds", "*", "*", "*")):
+            if os.path.basename(path) in ["rddl-parser", "search"]:
+                binaries.append(path)
+        subprocess.call(["strip"] + binaries)
+
+        # Compress src directory.
+        subprocess.call(
+            ["tar", "-cf", "src.tar", "--remove-files", "src"], cwd=self.path
+        )
+        subprocess.call(["xz", "src.tar"], cwd=self.path)
---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="prostlab",
+    version="1.0",
+    description="Use lab for experiments with the Prost planner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="experiment lab Prost",
+    author="Thomas Keller",
+    author_email="thomaskeller79@gmail.com",
+    url="https://github.com/prost-planner/prostlab",
+    license="GPL3+",
+    packages=["prostlab", "prostlab.reports"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=[
+        "lab>=5.5",
+    ],
+    python_requires=">=3.5",
+)
